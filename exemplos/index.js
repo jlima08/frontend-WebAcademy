6f71e9a740dd56fa7d3937569e64c5b8938745fb
jlima08 <johnlima540@gmail.com>
2025-07-07 20:24:46 -0500

    adicionando js

--- exemplos/index.js
+++ exemplos/index.js
@@ -1,1 +1,27 @@
-console.log("hello webacademy");
+
+let times = ["flu", "fla", "rb"];
+
+// for (let j = 0; j <times.length; j++){
+//     console.log(times[j])
+// }
+// for (time of times)
+// console.log(time);
+
+let estado = new Object();
+estado = {
+    nome: "Acre",
+    populacao: 906876,
+    capital:{
+        nome: "Rio branco",
+        populacao: 400000
+    },
+    indicadores: function(){
+        alert(`a capital do ${this.nome} é ${this.capital.nome} e tem ${this.capital.populacao} habitantes`);
+    }
+
+}
+// console.log(`a capital do ${estado.nome} é ${estado.capital.nome} e tem ${estado.capital.populacao} habitantes`);
+alert(estado.indicadores())
+
+
+
